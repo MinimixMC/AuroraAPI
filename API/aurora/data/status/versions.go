b415305479ca481a72fdbb3d24d2f009ef8ff40d
ImCarsen <carsen@imcarsen.com>
2024-05-18 20:14:11 -0600

    Use highest version for the name

--- API/aurora/data/status/versions.go
+++ API/aurora/data/status/versions.go
@@ -3,11 +3,11 @@ package status
 // Protocol version is the key
 var Version map[int]VersionInfo = map[int]VersionInfo{
 	766: {
-		Name:     "1.20.5-1.20.6",
+		Name:     "1.20.6",
 		Protocol: 766,
 	},
 	765: {
-		Name:     "1.20.3-1.20.4",
+		Name:     "1.20.4",
 		Protocol: 765,
 	},
 	764: {
@@ -15,7 +15,7 @@ var Version map[int]VersionInfo = map[int]VersionInfo{
 		Protocol: 764,
 	},
 	763: {
-		Name:     "1.20-1.20.1",
+		Name:     "1.20.1",
 		Protocol: 763,
 	},
 }
